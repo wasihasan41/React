5191a520488f0683ca6bcd7a605cbd6862db4c42
wasihasan41 <wasihasan41@gmail.com>
2024-09-17 17:43:37 +0530

    react

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
 import Demo from './react'
 
 function App() { //App is a component, it must be always start with capital letter
-// <> </> its called Fragment react can run 1 block code at a time so we use these instead of <div></div>
+  // <> </> its called Fragment react can run 1 block code at a time so we use these instead of <div></div>
+  const username = "React"; //creating variables // use {} for init vars; called evaluated expression as we cant write if else n all in this
   return (
-    <> 
-      <h1>My Coding Academia</h1> 
+    <>
+      <h1>My Coding Academia {username}</h1>  
       <Demo />
     </>
   )
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,8 +1,31 @@
 import { createRoot } from 'react-dom/client'
-import Demo from './react.jsx'
 import App from './App.jsx'
-import './index.css'
+import React from 'react'
+
+// function MyApp(){
+//   return(
+//     <div>
+//     <h1>Custom App</h1>
+//     </div>
+//   )
+// }
+
+const anotherElement = (
+  <a href="https://google.com">Visit Google</a>
+)
+
+const anotherUser = "React"
+
+const reactElement = React.createElement( // bable transpiler inject these
+  'a',
+  {
+    href: "https://www.google.com",
+    target: "_blank"
+  },
+  "Click me to visit google",
+  anotherUser //use variables here in evaluated expressions
+)
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  reactElement
 )
