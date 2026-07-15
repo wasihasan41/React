b22471127fb0aa4b37514710d50f4807ed330f38
wasi hasan <https://github.com/wasihasan41>
2026-07-15 19:17:33 +0530

    axios

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,20 @@
 import Demo from './react'
+import axios from 'axios'
 
 function App() { //App is a component, it must be always start with capital letter
   // <> </> its called Fragment react can run 1 block code at a time so we use these instead of <div></div>
+  async function handleClick() {
+    
+    console.log("h")
+    const data = await axios.get("https://jsonplaceholder.typicode.com/users")
+    console.log(data.data) 
+  }
   const username = "React"; //creating variables // use {} for init vars; called evaluated expression as we cant write if else n all in this
   return (
     <>
       <h1>My Coding Academia {username}</h1>  
-      <Demo />
+      <button onClick={handleClick}>Get data</button>
+
     </>
   )
 }
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -26,6 +26,4 @@ const reactElement = React.createElement( // bable transpiler inject these
   anotherUser //use variables here in evaluated expressions
 )
 
-createRoot(document.getElementById('root')).render(
-  reactElement
-)
+createRoot(document.getElementById('root')).render(App())
