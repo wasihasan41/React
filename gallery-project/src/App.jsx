0c69d2a3f3a7170b8444d65d4442cd96674e1b11
wasi hasan <https://github.com/wasihasan41>
2026-07-27 20:56:25 +0530

    complete

--- gallery-project/src/App.jsx
+++ gallery-project/src/App.jsx
@@ -1,54 +1,51 @@
-import {useState} from 'react'
-import {useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Footer from './components/Footer'
 
-function App() {
 
+function App() {
   const [images, setImages] = useState([])
   const [page, setPage] = useState(1)
-  const [height, setHeight] = useState(128)
-
+  const [selectedImage, setSelectedImage] = useState(null)
 
   const fetchData = async () => {
     try {
-    const response = await axios.get(`https://picsum.photos/v2/list?page=${page}&limit=9`)
-    const data = response.data
-    setImages(data)
-
-    }catch (error) {
-      console.error('Net khol bhai' )
+      const response = await axios.get(`https://picsum.photos/v2/list?page=${page}&limit=9`)
+      const data = response.data
+      setImages(data)
+    } catch (error) {
+      console.error('Net khol bhai')
     }
   }
-  useEffect(() => { fetchData()}, [page])
 
-  function zoom(){
-    
-    Object.assign(event.target.style, {
-      position: 'fixed', 
-      top: '50%',
-      left: '50%'
-    })
-    console.log(height)
-  }
-  document.body.addEventListener('click', small)
-  function small(){
-    setHeight(128)
-  }
+  useEffect(() => {
+    fetchData()
+  }, [page])
   return (
+    <>
     <div className="flex flex-wrap">
-      {images.map((img, i)=>{
-        return ( 
-          <div key={img.id} className="border border-amber-400 text-center m-1" onClick={zoom} >
-            <img className="w-49" src={img.download_url} alt={'image'} style={{height:`${height}px`, pointerEvents: 'none'}}  />
+      {images.map((img, i) => {
+        return (
+          <div key={img.id} className="border border-amber-400 text-center m-1" >
+            <img className="w-49" src={img.download_url} alt={'image'} style={{ height: `128px`}} onClick={()=>{setSelectedImage(img)}} />
             <h6>Image {(page - 1) * 9 + i + 1}</h6>
           </div>
-        )})
-      }
-      <Footer data={fetchData} setPage={setPage} page={page}/> 
-      
-
+        )
+      })}
     </div>
+    {
+      selectedImage && 
+      <div className='  inset-0 bg-black/60 backdrop-blur-sm h-screen fixed top-0 left-0 right-0 bottom-0 ' onClick={()=>{setSelectedImage(null)}}>
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-amber-400 border-2 h-[70%] w-[70%]">
+          <img src={selectedImage.download_url} className=" flex  h-[80%] w-full"/>
+          <h1 className='text-white text-2xl h-[20%] flex justify-center items-center'>Image {Number( selectedImage.id) + 1}</h1>
+          
+        </div>
+      </div>
+    }
+    <Footer data={fetchData} setPage={setPage} page={page} />
+    </>
+    
   )
 }
 
