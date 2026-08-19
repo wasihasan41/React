d45adc84ca348e7f57a9763c966e0cbedb58b45b
wasi hasan <https://github.com/wasihasan41>
2026-08-19 22:01:55 +0530

    updated

--- dynamic-routing-nextjs/app/page.tsx
+++ dynamic-routing-nextjs/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div style={{ padding: '20px' }}>
-      <h1>Welcome to Store 🛍️</h1>
+      <h1>Welcome to Students Database📚</h1>
       <Link href="/students">
         <button>View students</button>
       </Link>
